9b6861a696b0214fa1f37cfa5c78df2fd62cbbd3
jeshrz <jeshrz@foxmail.com>
2021-03-03 21:07:13 +0800

    add SVC_Handler

--- bsp/stm32/stm32f746-st-disco/applications/main.c
+++ bsp/stm32/stm32f746-st-disco/applications/main.c
@@ -17,17 +17,10 @@
 
 int main(void)
 {
-    int count = 1;
-    /* set LED1 pin mode to output */
-    rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
-
-    while (count++)
-    {
-        rt_pin_write(LED1_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
-        rt_pin_write(LED1_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
-    }
+    __asm__ __volatile__
+    (
+        "svc #11"
+    );
 
     return RT_EOK;
 }
